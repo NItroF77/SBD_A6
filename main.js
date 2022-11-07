3a8e64296f967478f0940a1de5977fbb8dd0093e
NItroF77 <raihanfs77@gmail.com>
2022-11-07 10:43:31 +0700

    initial release

---
+++ main.js
@@ -0,0 +1,46 @@
+// myscript.js
+// This example uses Node 8's async/await syntax.
+
+const oracledb = require('oracledb');
+const express = require('express');
+const app = express();
+app.set("view engine", "ejs");
+app.set("views","views");
+let sql,binds,options,result;
+let connection;
+app.listen(1522,() =>{
+    console.log("server ready");
+})
+
+app.get("/",(req,res) =>{
+    connectDb();
+    datas = JSON.parse(JSON.stringify(result["rows"]));
+    res.render("index",{datas:datas,title:"OracleDb Simulation"});
+})
+oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
+
+
+async function connectDb(){
+  try {
+    connection = await oracledb.getConnection( {
+      user          : "user1",
+      password      : "user1",
+      connectString : "localhost/xe"
+    });
+    sql = `SELECT * FROM no_example`;
+
+    binds = {};
+
+    // For a complete list of options see the documentation.
+    options = {
+      outFormat: oracledb.OUT_FORMAT_OBJECT,   // query result format
+      // extendedMetaData: true,               // get extra metadata
+      // prefetchRows:     100,                // internal buffer allocation size for tuning
+      // fetchArraySize:   100                 // internal buffer allocation size for tuning
+    };
+
+    result = await connection.execute(sql, binds, options);
+    console.log(result);
+  } catch (err) {
+    console.error(err);}
+  }
